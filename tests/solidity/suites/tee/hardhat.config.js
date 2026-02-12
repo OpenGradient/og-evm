80865fbee0dd111178f81132c37b3d57d986a306
balogh.adam@icloud.com <adambalogh@Adams-MacBook-Pro.local>
2026-02-12 19:10:19 +0100

    tee test config

--- tests/solidity/suites/tee/hardhat.config.js
+++ tests/solidity/suites/tee/hardhat.config.js
@@ -3,11 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [
-      {
-        version: "0.8.20",
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
-    ],
+    },
   },
   networks: {
     cosmos: {
